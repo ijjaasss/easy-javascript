5f0967458e694a00f2a9848cb3909c869d9e1486
ijjaasss <ijasmuthu04@gmail.com>
2025-01-08 15:26:36 +0530

    maximum value find

---
+++ maximum value of object.js
@@ -0,0 +1,28 @@
+//this is prototype
+Object.prototype.maxValue=function(){
+    const value=Object.values(this)
+    var temp=value.filter((val)=>typeof val!=='number')
+     if(temp.length>0){
+     throw 'only number is accept'
+     }
+  var max=Math.max(...value)
+  
+    for(let x in this){
+     if(this[x]==max){
+         return x
+     }
+    }
+ }
+
+
+ 
+ //working of my code 
+ var obj={
+     a:1,
+     b:5,
+     c:300,
+     d:40
+ }
+ console.log(obj.maxValue());  //answer is object key of maximum value
+
+ 
